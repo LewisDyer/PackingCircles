c4a29136933d31f308ee95f8290972932b306921
LewisDyer <lewisd251@gmail.com>
2020-06-20 14:43:24 +0100

    Added experimental animation capabilities

--- circle_packing.py
+++ circle_packing.py
@@ -1,6 +1,10 @@
 import cairo
 from random import uniform, choice
 from math import pi, sqrt
+
+import imageio
+from PIL import Image
+import numpy
 WIDTH = 10 # width in units
 HEIGHT = 10 # height in units
 PIXEL_SCALE = 100 # how many pixels per unit?
@@ -9,6 +13,8 @@ surface = cairo.ImageSurface(cairo.FORMAT_RGB24, WIDTH*PIXEL_SCALE, HEIGHT*PIXEL
 
 ctx = cairo.Context(surface)
 ctx.scale(PIXEL_SCALE, PIXEL_SCALE)
+
+gif_writer=imageio.get_writer("outputs/slower_gif.gif", mode="I")
 
 # background
 ctx.rectangle(0, 0, WIDTH, HEIGHT)
@@ -45,7 +51,7 @@ tint_params = {
     'max_circles': 5000,
     'max_attempts': 50,
     'padding': 0.05,
-    'colours': [(0, 0, 0, 255*0.25)]
+    'colours': [(0, 0, 0, 255*0.25)],
 }
 
 gradient_params = {
@@ -59,22 +65,47 @@ gradient_params = {
     'inner_proportion': 0.9,
     'inner': False,
     'clip_walls': False,
-    'is_gradient': True
+    'is_gradient': True,
 }
+
+def as_numpy_array(surface):
+
+    w = surface.get_width()
+    h = surface.get_height()
+    
+    data = surface.get_data()
+    
+    a = numpy.ndarray(shape=(h,w), dtype=numpy.uint32, buffer=data)
+    
+    i = Image.frombytes("RGBA", (w,h), a, "raw", "BGRA", 0, 1)
+    
+    return numpy.asarray(i)
+
+def add_image(writer, surface):
+    
+    a = as_numpy_array(surface)
+    writer.append_data(a)
 
 def render_circle_layer(p):
     circles = []
     for i in range(p['max_circles']):
-        draw_circle(p, circles)
+        make_circle(p, circles)
 
 class Circle:
     def __init__(self, min_radius, colours):
         self.x = uniform(0, WIDTH)
         self.y = uniform(0, HEIGHT)
         self.radius = min_radius
-        self.r, self.g, self.b, self.a = choice(colours)
+        sc = choice(colours)
+        if len(colours) > 1:
+            ec = choice(colours)
+            while sc == ec:
+                ec = choice(colours)
+            self.er, self.eg, self.eb, self.ea = ec
+        self.r, self.g, self.b, self.a = sc
+        
 
-def draw_circle(p, circles):
+def make_circle(p, circles):
     place_to_draw = False
     for i in range(p['max_attempts']):
         circle = Circle(p['min_radius'], p['colours'])
@@ -88,27 +119,30 @@ def draw_circle(p, circles):
     if not place_to_draw:
         return
 
+
+    start_animation = 0.75
     while(circle.radius < p['max_radius']):
-        circle.radius += 0.01
+        circle.radius += start_animation
         if check_collision(circle, circles,p):
-            circle.radius -= 0.01
-            break
+            circle.radius -= start_animation
+            start_animation /= 2
+            if start_animation < 0.01:
+                break
+        else:
+            render_circle(circle, p)
 
     circles.append(circle)
+
+def render_circle(circle, p):
     if circle.radius >= p.get('threshold_radius', 0):
         ctx.move_to(circle.x+circle.radius, circle.y)
         ctx.arc(circle.x, circle.y, circle.radius, 0, 2*pi)
 
 
-    if p.get('is_gradient', False) and len(p['colours']) > 1:
-        sc = choice(p['colours'])
-        ec = choice(p['colours'])
-        while sc == ec:
-            ec = choice(p['colours'])
-
+    if p.get('is_gradient', False):
         pattern = cairo.LinearGradient(circle.x, circle.y - circle.radius, circle.x, circle.y + circle.radius)
-        pattern.add_color_stop_rgba(0, sc[0]/255, sc[1]/255, sc[2]/255, sc[3]/255)
-        pattern.add_color_stop_rgba(1, ec[0]/255, ec[1]/255, ec[2]/255, ec[3]/255)
+        pattern.add_color_stop_rgba(0, circle.r/255, circle.g/255, circle.b/255, circle.a/255)
+        pattern.add_color_stop_rgba(1, circle.er/255, circle.eg/255, circle.eb/255, circle.ea/255)
         ctx.set_source(pattern)
     else:
         ctx.set_source_rgba(circle.r/255, circle.g/255, circle.b/255, circle.a/255)
@@ -119,7 +153,10 @@ def draw_circle(p, circles):
         ctx.arc(circle.x, circle.y, p['inner_proportion']*circle.radius, 0, 2*pi)
         tint = choice(p['inner_colours'])
         ctx.set_source_rgba(tint[0]/255, tint[1]/255, tint[2]/255, tint[3]/255)
-        ctx.fill() 
+        ctx.fill()
+    
+    add_image(gif_writer, surface)
+    
 
 def check_collision(circle, circles, p):
     for other_circle in circles:
@@ -141,6 +178,8 @@ def check_collision(circle, circles, p):
 
 render_circle_layer(gradient_params)
 
-render_circle_layer(tint_params)
+for i in range(30):
+    add_image(gif_writer, surface)
+#render_circle_layer(tint_params)
 
 surface.write_to_png('outputs/circle_packing.png')
